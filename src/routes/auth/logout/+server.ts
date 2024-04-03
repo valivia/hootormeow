8c6fded87c6b4d780ba6fd57164d92538016518d
valivia <26413018+valivia@users.noreply.github.com>
2024-04-03 18:00:26 +0000

    ✨ Added signup toggle

--- src/routes/auth/logout/+server.ts
+++ src/routes/auth/logout/+server.ts
@@ -2,9 +2,10 @@ import { redirect, type RequestHandler } from "@sveltejs/kit";
 import { logger } from "lib/server/logger";
 import { ensureLoggedIn } from "lib/server/session";
 
-export const GET: RequestHandler = async ({ cookies }) => {
+export const GET: RequestHandler = async ({ request, cookies }) => {
     const user = await ensureLoggedIn(cookies);
+    const ip = request.headers.get("cf-connecting-ip");
     cookies.set("sessionToken", "", { path: "/", expires: new Date(0) });
-    logger.info(`Logged out user ${user.displayName}`, { user });
+    logger.info(`User log out: ${user.displayName} - ${user.id} (${ip})`, { user, ip });
     redirect(302, "/");
 };
--- src/routes/auth/redirect/+server.ts
+++ src/routes/auth/redirect/+server.ts
@@ -5,7 +5,7 @@ import { redirect, type RequestHandler } from "@sveltejs/kit";
 import { nanoid } from "nanoid";
 import { DISCORD_TOKEN_URL, DISCORD_USER_URL } from "lib/auth";
 import { prisma } from "lib/server/prisma";
-import { PUBLIC_DISCORD_CLIENT_ID, PUBLIC_DISCORD_REDIRECT_URI } from "$env/static/public";
+import { PUBLIC_ALLOW_SIGNUP, PUBLIC_DISCORD_CLIENT_ID, PUBLIC_DISCORD_REDIRECT_URI } from "$env/static/public";
 import { DISCORD_CLIENT_SECRET } from "$env/static/private";
 import { logger } from "lib/server/logger";
 
@@ -48,32 +48,59 @@ export const GET: RequestHandler = async ({ url, cookies, request }) => {
     if (!userResponse.ok)
         return new Response("Failed to get user information from Discord");
 
+    type UserJson = {
+        id: string,
+        username: string,
+        avatar: string,
+        banner: string,
+        accent_color: string,
+        banner_color: string,
+        global_name: string,
+    }
     // https://discord.com/developers/docs/topics/oauth2#get-current-authorization-information
-    const userJson = await userResponse.json();
+    const responseJson = await userResponse.json();
+    const userJson = responseJson.user as UserJson;
     const newToken = nanoid(32);
-    if (!userJson.user.displayName) {
-        userJson.user.displayName = userJson.user.username;
+
+    if (!userJson.global_name) userJson.global_name = userJson.username;
+
+    let user = await prisma.user.findUnique({ where: { id: userJson.id } });
+    const ip = request.headers.get("cf-connecting-ip");
+
+    if (!user) {
+        if (PUBLIC_ALLOW_SIGNUP !== "true") {
+            logger.info(`Sign up is disabled: ${userJson.global_name} - ${userJson.id} (${ip})`, { userJson, ip });
+            return new Response("Sign up is disabled");
+        }
+
+        user = await prisma.user.create({
+            data: {
+                id: userJson.id,
+                token: newToken,
+                displayName: userJson.global_name,
+                userName: userJson.username,
+                avatar: userJson.avatar,
+            }
+        });
+        logger.info(`User create: ${user.displayName} - ${userJson.id} (${ip})`, { user, ip });
+    } else if (
+        userJson.avatar !== user.avatar
+        || userJson.global_name !== user.displayName
+        || userJson.username !== user.userName
+    ) {
+        user = await prisma.user.update({
+            where: { id: userJson.id },
+            data: {
+                displayName: userJson.global_name,
+                userName: userJson.username,
+                avatar: userJson.avatar,
+            }
+        });
+        logger.info(`User update: ${user.displayName} - ${user.id} (${ip})`, { user, ip });
+    } else {
+        logger.info(`User log in: ${user.displayName} - ${user.id} (${ip})`, { user, ip });
     }
 
-    const user = await prisma.user.upsert({
-        where: { id: userJson.user.id },
-        create: {
-            id: userJson.user.id,
-            token: newToken,
-            displayName: userJson.user.global_name,
-            userName: userJson.user.username,
-            avatar: userJson.user.avatar,
-        },
-        update: {
-            displayName: userJson.user.global_name,
-            userName: userJson.user.username,
-            avatar: userJson.user.avatar,
-        }
-    });
-
-
-    const ip = request.headers.get("cf-connecting-ip");
-    logger.info(`Logged in user ${user.displayName} - ${ip}`, { user, ip });
     const tokenExpirationDate = new Date(Date.now() + (60 * 60 * 24 * 1000));
     cookies.set("sessionToken", user.token, { path: "/", expires: tokenExpirationDate });
     redirect(302, "/");
